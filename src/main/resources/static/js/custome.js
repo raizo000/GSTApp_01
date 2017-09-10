51881e8530d36d8109318ed0211af7a24e0f53db
raizo000 <jenova2010.bn@gmail.com>
2017-09-10 21:47:14 +0700

    Update

--- src/main/resources/static/js/custome.js
+++ src/main/resources/static/js/custome.js
@@ -1,62 +1,3 @@
-$(document).ready(function () {
-    //create arrays to store option and values
-    var khoi10 = [{
-        display: "10A1",
-        value: "10A1"
-    },
-        {
-            display: "10A2",
-            value: "10A2"
-        },
-        {
-            display: "10A3",
-            value: "10A3"
-        },
-
-    ];
-
-    var khoi11 = [{
-        display: "11A1",
-        value: "11A1"
-    },
-        {
-            display: "11A2",
-            value: "11A2"
-        },
-        {
-            display: "11A3",
-            value: "11A3"
-        },
-    ];
-
-    //If parent option is changed
-    $("#parent_selection").change(function () {
-        var parent = $(this).val(); //get option value from parent
-        switch (parent) { //using switch compare selected option and populate child
-            case 'khoi10':
-                $('#child_selection').attr('disabled', false);
-                list(khoi10);
-                break;
-            case 'khoi11':
-                $('#child_selection').attr('disabled', false);
-                list(khoi11);
-                break;
-            default: //default child option is blank
-                $("#child_selection").html('');
-                $('#child_selection').attr("disabled", true);
-                break;
-        }
-    });
-
-    //function to populate child select box
-    function list(array_list) {
-        $("#child_selection").html(""); //reset child options
-        $(array_list).each(function (i) { //populate child options
-            $("#child_selection").append("<option value=" + array_list[i].value + ">" +
-                array_list[i].display + "</option>");
-        });
-    }
-});
 
 
 $('#birthday').daterangepicker({
@@ -86,3 +27,7 @@ function validatePassword() {
 
 password.onchange = validatePassword;
 confirm_password.onkeyup = validatePassword;
+
+function resetFunction() {
+    document.getElementById("form").reset();
+}
